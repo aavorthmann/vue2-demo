3a25d908781f13264f472f988273545678a5a0cc
Allison Vorthmann <46933161+aavorthmann@users.noreply.github.com>
2023-12-16 01:34:09 -0600

    feat(test): i am going cray cray

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,5 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:8080',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+  defaultCommandTimeout: 10000,
+})
