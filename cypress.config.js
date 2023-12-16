c1beb34875552b558565a3383bfd8ab4cc6d5cf5
Allison Vorthmann <46933161+aavorthmann@users.noreply.github.com>
2023-12-16 02:05:45 -0600

    feat(test): i hate this

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8080',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
